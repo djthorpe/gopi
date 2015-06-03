09638ca227b805024ef6330319cd4327132f5a53
David Thorpe <djt@mutablelogic.com>
2015-06-03 10:06:32 +0000

    Added files

---
+++ examples/blink.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/djthorpe/gopi/gpio"
+)
+
+func main() {
+	// set GPIO25 to output mode
+	pin, err := gpio.OpenPin(rpi.GPIO25, gpio.ModeOutput)
+	if err != nil {
+		fmt.Printf("Error opening pin! %s\n", err)
+		return
+	}
+
+	// turn the led off on exit
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			fmt.Printf("\nClearing and unexporting the pin.\n")
+			pin.Clear()
+			pin.Close()
+			os.Exit(0)
+		}
+	}()
+
+	for {
+		pin.Set()
+		time.Sleep(100 * time.Millisecond)
+		pin.Clear()
+		time.Sleep(100 * time.Millisecond)
+	}
+}
