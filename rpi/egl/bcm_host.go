b191d6f23911f2203f8b4ba47df1f4fc336ffc21
David Thorpe <djt@mutablelogic.com>
2015-12-20 11:19:08 +0000

    Updated

---
+++ rpi/egl/bcm_host.go
@@ -0,0 +1,17 @@
+package egl
+
+/*
+	#include "bcm_host.h"
+*/
+
+import "C"
+
+func BCMHostInit() {
+	C.bcm_host_init()
+}
+
+func GraphicsGetDisplaySize(displayNumber uint16) (uint32,uint32) {
+	var w,h uint32
+	C.graphics_get_display_size((C.uint16_t)(displayNumber),(*C.uint32_t)(&w),(*C.uint32_t)(&h))
+	return w,h
+}
