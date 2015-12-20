75f7fad8cb8b58e47fbd43073ad20bd33b297377
David Thorpe <djt@mutablelogic.com>
2015-12-20 12:23:43 +0000

    Updated

--- examples/hellotriangle/hellotriangle.go
+++ examples/hellotriangle/hellotriangle.go
@@ -11,10 +11,10 @@ func main() {
 
 	// Initalize display
 	display := egl.GetDisplay()
-	if err := egl.Initialize(display,nil,nil); err != nil {
+/*	if err := egl.Initialize(display,nil,nil); err != nil {
 		log.Fatalf("Unable to initalize display: %v",err)
 	}
-
+*/
 	// Terminate display
 	if err := egl.Terminate(display); err != nil {
 		log.Fatalf("Unable to terminate display: %v",err)
