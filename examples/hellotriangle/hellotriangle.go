469472292cd34d76a00d3822daf3acde2085221b
David Thorpe <djt@mutablelogic.com>
2015-12-20 12:44:00 +0000

    Updated

--- examples/hellotriangle/hellotriangle.go
+++ examples/hellotriangle/hellotriangle.go
@@ -5,44 +5,47 @@ import (
 	"log"
 )
 
-
 func main() {
 	egl.BCMHostInit()
 
 	// Initalize display
 	display := egl.GetDisplay()
-	if err := egl.Initialize(display,nil,nil); err != nil {
-		log.Fatalf("Unable to initalize display: %v",err)
+	if err := egl.Initialize(display, nil, nil); err != nil {
+		log.Fatalf("Unable to initalize display: %v", err)
 	}
 
 	// Choose configuration
-	var attr = []int32{
-		egl.RED_SIZE,8,
-		egl.GREEN_SIZE,8,
-		egl.BLUE_SIZE,8,
-		egl.ALPHA_SIZE,8,
-		egl.SURFACE_TYPE,egl.WINDOW_BIT,
+	attr := []int32{
+		egl.EGL_RED_SIZE, 8,
+		egl.EGL_GREEN_SIZE, 8,
+		egl.EGL_BLUE_SIZE, 8,
+		egl.EGL_ALPHA_SIZE, 8,
+		egl.EGL_SURFACE_TYPE, egl.EGL_WINDOW_BIT,
 		egl.NONE,
 	}
-	if err := egl.ChooseConfig(display,attr,&config,1,&numConfig); err != nil {
-		log.Fatalf("Unable to initalize display: %v",err)
+
+	var (
+		config    egl.Config
+		numConfig int32
+	)
+
+	if err := egl.ChooseConfig(display, attr, &config, 1, &numConfig); err != nil {
+		log.Fatalf("Unable to initalize display: %v", err)
 	}
 
-/*
-	if ok := egl.GetConfigAttrib(display, config, egl.NATIVE_VISUAL_ID, &vid); !ok {
-		egl.LogError(egl.GetError())
-	}
-	egl.BindAPI(egl.OPENGL_ES_API)
-	context = egl.CreateContext(display, config, egl.NO_CONTEXT, &ctxAttr[0])
+	/*
+		if ok := egl.GetConfigAttrib(display, config, egl.NATIVE_VISUAL_ID, &vid); !ok {
+			egl.LogError(egl.GetError())
+		}
+		egl.BindAPI(egl.OPENGL_ES_API)
+		context = egl.CreateContext(display, config, egl.NO_CONTEXT, &ctxAttr[0])
 
-	screen_width, screen_height = egl.GraphicsGetDisplaySize(0)
-	log.Printf("Display size W: %d H: %d\n", screen_width, screen_height)
-*/
+		screen_width, screen_height = egl.GraphicsGetDisplaySize(0)
+		log.Printf("Display size W: %d H: %d\n", screen_width, screen_height)
+	*/
 
 	// Terminate display
 	if err := egl.Terminate(display); err != nil {
-		log.Fatalf("Unable to terminate display: %v",err)
+		log.Fatalf("Unable to terminate display: %v", err)
 	}
 }
-
-
