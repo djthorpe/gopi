4c5b8d361274426f40b12841c3d8f10103f4ec49
David Thorpe <djt@mutablelogic.com>
2015-12-20 11:35:27 +0000

    Updated

--- rpi/egl/egl.go
+++ rpi/egl/egl.go
@@ -9,7 +9,7 @@ package egl
 
 /*
 	#cgo CFLAGS:   -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads
-	#cgo LDFLAGS:  -L/opt/vc/lib -lGLESv2
+	#cgo LDFLAGS:  -L/opt/vc/lib -lEGL
 	#include "EGL/egl.h"
 */
 import "C"
