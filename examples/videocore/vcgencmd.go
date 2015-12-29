7ed15de2d7d1a619240788b56ab221496cee0a38
David Thorpe <djt@mutablelogic.com>
2015-12-29 10:07:52 +0000

    Updated revision code

--- examples/videocore/vcgencmd.go
+++ examples/videocore/vcgencmd.go
@@ -27,7 +27,7 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////
 
-var commandmap = map[string]func(*rpi.State){
+var commandmap = map[string]func(*rpi.State) error {
 	"all":      allCommand,
 	"temp":     tempCommand,
 	"clocks":   clocksCommand,
@@ -42,130 +42,173 @@ var commandmap = map[string]func(*rpi.State){
 
 ////////////////////////////////////////////////////////////////////////////
 
-func allCommand(pi *rpi.State) {
-	tempCommand(pi)
-	clocksCommand(pi)
-	voltsCommand(pi)
-	memoryCommand(pi)
-	codecsCommand(pi)
-	otpCommand(pi)
-	serialCommand(pi)
-	revisionCommand(pi)
-	modelCommand(pi)
+func allCommand(pi *rpi.State) error {
+	if err := tempCommand(pi); err != nil {
+		return err
+	}
+
+	if err := clocksCommand(pi); err != nil {
+		return err
+	}
+
+	if err := voltsCommand(pi); err != nil {
+		return err
+	}
+
+	if err := memoryCommand(pi); err != nil {
+		return err
+	}
+
+	if err := codecsCommand(pi); err != nil {
+		return err
+	}
+
+	if err := otpCommand(pi); err != nil {
+		return err
+	}
+
+	if err := serialCommand(pi); err != nil {
+		return err
+	}
+
+	if err := revisionCommand(pi); err != nil {
+		return err
+	}
+
+	if err := modelCommand(pi); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func tempCommand(pi *rpi.State) {
+func tempCommand(pi *rpi.State) error {
 	// print out temperature
 	coretemp, err := pi.GetCoreTemperatureCelcius()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Printf("Temperature=%vºC\n", coretemp)
+	return nil
 }
 
-func clocksCommand(pi *rpi.State) {
+func clocksCommand(pi *rpi.State) error {
 	// print out clocks
 	clocks, err := pi.GetClockFrequencyHertz()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Println("Clock Frequency")
 	for k, v := range clocks {
 		fmt.Printf("  %v=%vMHz\n", k, (float64(v) / 1E6))
 	}
+
+	return nil
 }
 
-func voltsCommand(pi *rpi.State) {
+func voltsCommand(pi *rpi.State) error {
 	// print out volts
 	volts, err := pi.GetVolts()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Println("Volts")
 	for k, v := range volts {
 		fmt.Printf("  %v=%vV\n", k, v)
 	}
+
+	return nil
 }
 
-func memoryCommand(pi *rpi.State) {
+func memoryCommand(pi *rpi.State) error {
 	// print out memory sizes
 	memory, err := pi.GetMemoryMegabytes()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Println("Memory")
 	for k, v := range memory {
 		fmt.Printf("  %v=%vMB\n", k, v)
 	}
+
+	return nil
 }
 
-func codecsCommand(pi *rpi.State) {
+func codecsCommand(pi *rpi.State) error {
 	// print out codecs
 	codecs, err := pi.GetCodecs()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Println("Codecs")
 	for k, v := range codecs {
 		fmt.Printf("  %v=%v\n", k, v)
 	}
+
+	return nil
 }
 
-func otpCommand(pi *rpi.State) {
+func otpCommand(pi *rpi.State) error {
 	// print out OTP memory
 	otp, err := pi.GetOTP()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Println("OTP")
 	for i, v := range otp {
 		fmt.Printf("  %02d=%08X\n", i, v)
 	}
+
+	return nil
 }
 
-func serialCommand(pi *rpi.State) {
+func serialCommand(pi *rpi.State) error {
 	// print out Serial number
 	serial, err := pi.GetSerial()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Printf("Serial=%016X\n", serial)
+
+	return nil
 }
 
-func revisionCommand(pi *rpi.State) {
+func revisionCommand(pi *rpi.State) error {
 	// print out Revision
 	revision, err := pi.GetRevision()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
 	fmt.Printf("Revision=%08X\n", revision)
+
+	return nil
 }
 
-func modelCommand(pi *rpi.State) {
-	// print out Revision
+func modelCommand(pi *rpi.State) error {
+	// print out Model information
 	model, err := pi.GetModel()
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(-1)
+		return err
 	}
 
-	fmt.Printf("Model=%+v\n", model)
+	fmt.Println("Model")
+	fmt.Printf("  Revision=0x%08X\n",model.Revision)
+	fmt.Printf("  Product=Respberry Pi %v (%v)\n",model.ProductString,model.Product)
+	fmt.Printf("  Processor=%v (%v)\n",model.ProcessorString,model.Processor)
+	fmt.Printf("  Memory=%vM\n",model.MemoryMB)
+	fmt.Printf("  Manufacturer=%v (%v)\n",model.ManufacturerString,model.Manufacturer)
+	fmt.Printf("  PCB Revision=%v\n",model.PCBRevision)
+	fmt.Printf("  Peripheral Base=0x%08X\n",model.PeripheralBase)
+
+	return nil
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -203,19 +246,21 @@ func main() {
 
 	////////////////////////////////////////////////////////////////////////////
 
-	var done bool
-
-	// attempt to run a custom command
-	if len(args) == 1 {
+	var err error
+	if len(args) == 1  && commandmap[args[0]] != nil {
 		if f := commandmap[args[0]]; f != nil {
-			f(pi)
-			done = true
+			err = f(pi)
+		}
+	} else {
+		var value string
+		value,err := rpi.VCGenCmd(strings.Join(args, " "))
+		if err == nil {
+			fmt.Println(value)
 		}
 	}
 
-	// if custom command not run, use VCGenCmd
-	if done == false {
-		fmt.Println(rpi.VCGenCmd(strings.Join(args, " ")))
+	if err != nil {
+		fmt.Fprintf(os.Stderr,"%v\n",err)
+		os.Exit(-1)
 	}
-
 }
--- rpi/revision.go
+++ rpi/revision.go
@@ -9,6 +9,30 @@ package rpi
 
 ////////////////////////////////////////////////////////////////////////////////
 
+type Product uint32
+type PCBRevision uint32
+type Processor uint32
+type Manufacturer uint32
+type MemoryMB uint32
+type PeripheralBase uintptr
+
+type Model struct {
+	Revision uint32
+	WarrantyBit bool
+	Product Product
+	PCBRevision PCBRevision
+	Processor Processor
+	Manufacturer Manufacturer
+	MemoryMB MemoryMB
+	PeripheralBase PeripheralBase
+
+	ProductString string
+	ProcessorString string
+	ManufacturerString string
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 const (
 	RPI_REVISION_WARRANTY_MASK     = 0x03000000
 	RPI_REVISION_ENCODING_MASK     = 0x00800000
@@ -17,6 +41,12 @@ const (
 	RPI_REVISION_PROCESSOR_MASK    = 0x0000F000
 	RPI_REVISION_MANUFACTURER_MASK = 0x000F0000
 	RPI_REVISION_MEMORY_MASK       = 0x00700000
+)
+
+const (
+	RPI_PERIPHERAL_BASE_UNKNOWN = 0x00000000
+	RPI_PERIPHERAL_BASE_BCM2835 = 0x20000000
+	RPI_PERIPHERAL_BASE_BCM2836 = 0x3F000000
 )
 
 const (
@@ -33,8 +63,8 @@ const (
 
 const (
     RPI_PROCESSOR_UNKNOWN = iota
-	RPI_PROCESSOR_BROADCOM_2835
-    RPI_PROCESSOR_BROADCOM_2836
+	RPI_PROCESSOR_BCM2835
+    RPI_PROCESSOR_BCM2836
 )
 
 const (
@@ -54,7 +84,7 @@ const (
 
 ////////////////////////////////////////////////////////////////////////////////
 
-var productmap1 = map[uint32]uint32{
+var productmap1 = map[uint32]Product{
 	0x02: RPI_MODEL_B,
 	0x03: RPI_MODEL_B,
 	0x04: RPI_MODEL_B,
@@ -74,7 +104,7 @@ var productmap1 = map[uint32]uint32{
 	0x15: RPI_MODEL_A_PLUS,
 }
 
-var pcbmap1 = map[uint32]uint32{
+var pcbmap1 = map[uint32]PCBRevision {
 	0x02: 1,
 	0x03: 1,
 	0x04: 2,
@@ -94,7 +124,7 @@ var pcbmap1 = map[uint32]uint32{
 	0x15: 1,
 }
 
-var memorymap1 = map[uint32]uint32{
+var memorymap1 = map[uint32]MemoryMB{
 	0x02: RPI_MEMORY_256MB,
 	0x03: RPI_MEMORY_256MB,
 	0x04: RPI_MEMORY_256MB,
@@ -114,27 +144,27 @@ var memorymap1 = map[uint32]uint32{
 	0x15: RPI_MEMORY_256MB,
 }
 
-var processormap1 = map[uint32]uint32{
-	0x02: RPI_PROCESSOR_BROADCOM_2835,
-	0x03: RPI_PROCESSOR_BROADCOM_2835,
-	0x04: RPI_PROCESSOR_BROADCOM_2835,
-	0x05: RPI_PROCESSOR_BROADCOM_2835,
-	0x06: RPI_PROCESSOR_BROADCOM_2835,
-	0x07: RPI_PROCESSOR_BROADCOM_2835,
-	0x08: RPI_PROCESSOR_BROADCOM_2835,
-	0x09: RPI_PROCESSOR_BROADCOM_2835,
-	0x0D: RPI_PROCESSOR_BROADCOM_2835,
-	0x0E: RPI_PROCESSOR_BROADCOM_2835,
-	0x0F: RPI_PROCESSOR_BROADCOM_2835,
-	0x10: RPI_PROCESSOR_BROADCOM_2835,
-	0x11: RPI_PROCESSOR_BROADCOM_2835,
-	0x12: RPI_PROCESSOR_BROADCOM_2835,
-	0x13: RPI_PROCESSOR_BROADCOM_2835,
-	0x14: RPI_PROCESSOR_BROADCOM_2835,
-	0x15: RPI_PROCESSOR_BROADCOM_2835,
+var processormap1 = map[uint32]Processor{
+	0x02: RPI_PROCESSOR_BCM2835,
+	0x03: RPI_PROCESSOR_BCM2835,
+	0x04: RPI_PROCESSOR_BCM2835,
+	0x05: RPI_PROCESSOR_BCM2835,
+	0x06: RPI_PROCESSOR_BCM2835,
+	0x07: RPI_PROCESSOR_BCM2835,
+	0x08: RPI_PROCESSOR_BCM2835,
+	0x09: RPI_PROCESSOR_BCM2835,
+	0x0D: RPI_PROCESSOR_BCM2835,
+	0x0E: RPI_PROCESSOR_BCM2835,
+	0x0F: RPI_PROCESSOR_BCM2835,
+	0x10: RPI_PROCESSOR_BCM2835,
+	0x11: RPI_PROCESSOR_BCM2835,
+	0x12: RPI_PROCESSOR_BCM2835,
+	0x13: RPI_PROCESSOR_BCM2835,
+	0x14: RPI_PROCESSOR_BCM2835,
+	0x15: RPI_PROCESSOR_BCM2835,
 }
 
-var manufacturermap1 = map[uint32]uint32{
+var manufacturermap1 = map[uint32]Manufacturer{
 	0x04: RPI_MANUFACTURER_SONY,
 	0x05: RPI_MANUFACTURER_QISDA,
 	0x06: RPI_MANUFACTURER_EGOMAN,
@@ -152,7 +182,7 @@ var manufacturermap1 = map[uint32]uint32{
 	0x15: RPI_MANUFACTURER_SONY,
 }
 
-var manufacturermap2 = map[uint32]uint32{
+var manufacturermap2 = map[uint32]Manufacturer{
 	0<<16: RPI_MANUFACTURER_SONY,
 	1<<16: RPI_MANUFACTURER_EGOMAN,
 	2<<16: RPI_MANUFACTURER_EMBEST,
@@ -160,7 +190,7 @@ var manufacturermap2 = map[uint32]uint32{
 	4<<16: RPI_MANUFACTURER_EMBEST,
 }
 
-var productmap2 = map[uint32]uint32{
+var productmap2 = map[uint32]Product{
 	0<<4: RPI_MODEL_A,
 	1<<4: RPI_MODEL_B,
 	2<<4: RPI_MODEL_A_PLUS,
@@ -173,18 +203,18 @@ var productmap2 = map[uint32]uint32{
 	9<<4: RPI_MODEL_ZERO,
 }
 
-var processormap2 = map[uint32]uint32{
-	0<<12: RPI_PROCESSOR_BROADCOM_2835,
-	1<<12: RPI_PROCESSOR_BROADCOM_2836,
+var processormap2 = map[uint32]Processor{
+	0<<12: RPI_PROCESSOR_BCM2835,
+	1<<12: RPI_PROCESSOR_BCM2836,
 }
 
-var memorymap2 = map[uint32]uint32{
+var memorymap2 = map[uint32]MemoryMB{
 	0<<20: RPI_MEMORY_256MB,
 	1<<20: RPI_MEMORY_512MB,
 	2<<20: RPI_MEMORY_1024MB,
 }
 
-var productstringmap = map[uint32]string{
+var productstringmap = map[Product]string{
     RPI_MODEL_UNKNOWN: "unknown",
 	RPI_MODEL_A: "A",
     RPI_MODEL_B: "B",
@@ -196,34 +226,24 @@ var productstringmap = map[uint32]string{
     RPI_MODEL_ZERO: "zero",
 }
 
-var processorstringmap = map[uint32]string{
+var processorstringmap = map[Processor]string{
 	RPI_PROCESSOR_UNKNOWN: "unknown",
-	RPI_PROCESSOR_BROADCOM_2835: "BCM2835",
-	RPI_PROCESSOR_BROADCOM_2836: "BCM2836",
+	RPI_PROCESSOR_BCM2835: "BCM2835",
+	RPI_PROCESSOR_BCM2836: "BCM2836",
 }
 
-var manufacturerstringmap = map[uint32]string{
+var peripheralbasemap = map[Processor]PeripheralBase{
+	RPI_PROCESSOR_UNKNOWN: RPI_PERIPHERAL_BASE_UNKNOWN,
+	RPI_PROCESSOR_BCM2835: RPI_PERIPHERAL_BASE_BCM2835,
+	RPI_PROCESSOR_BCM2836: RPI_PERIPHERAL_BASE_BCM2836,
+}
+
+var manufacturerstringmap = map[Manufacturer]string{
     RPI_MANUFACTURER_UNKNOWN: "unknown",
     RPI_MANUFACTURER_SONY: "Sony",
     RPI_MANUFACTURER_EGOMAN: "Egoman",
     RPI_MANUFACTURER_QISDA: "Qisda",
     RPI_MANUFACTURER_EMBEST: "Embest",
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-type Model struct {
-	Revision uint32
-	WarrantyBit bool
-	Product uint32
-	PCBRevision uint32
-	Processor uint32
-	Manufacturer uint32
-	MemoryMB uint32
-
-	ProductString string
-	ProcessorString string
-	ManufacturerString string
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -253,20 +273,22 @@ func (this *State) GetModel() (*Model, error) {
     revision = revision & ^w
 	if (revision & RPI_REVISION_ENCODING_MASK) != 0 {
 		// Raspberry Pi 2 style revision coding
-		model.Product = productmap2[revision & RPI_REVISION_PRODUCT_MASK]
-		model.PCBRevision = revision & RPI_REVISION_PCB_MASK
-		model.Processor = processormap2[revision & RPI_REVISION_PROCESSOR_MASK]
-		model.Manufacturer = manufacturermap2[revision & RPI_REVISION_MANUFACTURER_MASK]
-		model.MemoryMB = memorymap2[revision & RPI_REVISION_MEMORY_MASK]
+		model.Product = Product(productmap2[revision & RPI_REVISION_PRODUCT_MASK])
+		model.PCBRevision = PCBRevision(revision & RPI_REVISION_PCB_MASK)
+		model.Processor = Processor(processormap2[revision & RPI_REVISION_PROCESSOR_MASK])
+		model.Manufacturer = Manufacturer(manufacturermap2[revision & RPI_REVISION_MANUFACTURER_MASK])
+		model.MemoryMB = MemoryMB(memorymap2[revision & RPI_REVISION_MEMORY_MASK])
 	} else {
 		// Raspberry Pi 1 style revision coding
-		model.Product = productmap1[revision]
-		model.PCBRevision = pcbmap1[revision]
-		model.Processor = processormap1[revision]
-		model.Manufacturer = manufacturermap1[revision]
-		model.MemoryMB = memorymap1[revision]
+		model.Product = Product(productmap1[revision])
+		model.PCBRevision = PCBRevision(pcbmap1[revision])
+		model.Processor = Processor(processormap1[revision])
+		model.Manufacturer = Manufacturer(manufacturermap1[revision])
+		model.MemoryMB = MemoryMB(memorymap1[revision])
 	}
 
+	// set other members of the struct
+	model.PeripheralBase = PeripheralBase(peripheralbasemap[model.Processor])
 	model.ProductString = productstringmap[model.Product]
 	model.ProcessorString = processorstringmap[model.Processor]
 	model.ManufacturerString = manufacturerstringmap[model.Manufacturer]
