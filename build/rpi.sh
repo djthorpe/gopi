c745ff5df98aa46b97ff083a46fab444b6c73dc4
David Thorpe <djt@mutablelogic.com>
2016-08-27 12:05:28 +0100

    Reorg

---
+++ build/rpi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##############################################################
+# RPI BUILD SCRIPT
+##############################################################
+
+CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GO=`which go`
+LDFLAGS="-w -s"
+cd "${CURRENT_PATH}/.."
+
+##############################################################
+# Sanity checks
+
+if [ ! -d ${CURRENT_PATH} ] ; then
+  echo "Not found: ${CURRENT_PATH}" >&2
+  exit -1
+fi
+if [ "${GO}" == "" ] || [ ! -x ${GO} ] ; then
+  echo "go not installed or executable" >&2
+  exit -1
+fi
+
+##############################################################
+# go get dependencies
+
+##############################################################
+# install
+
+go install -ldflags "${LDFLAGS}" cmd/vcgencmd.go
+
+
---
+++ device/rpi/rpi.go
@@ -0,0 +1,77 @@
+/*
+	Go Language Raspberry Pi Interface
+	(c) Copyright David Thorpe 2016
+	All Rights Reserved
+
+	For Licensing and Usage information, please see LICENSE.md
+*/
+package rpi
+
+/*
+    #cgo CFLAGS: -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads
+    #cgo LDFLAGS: -L/opt/vc/lib -lbcm_host
+    #include "bcm_host.h"
+	#include "vc_vchi_gencmd.h"
+
+	int vc_gencmd_wrapper(char* response,int maxlen,const char* command) {
+		return vc_gencmd(response,maxlen,command);
+	}
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+const (
+	GENCMD_BUFFER_SIZE      = 1024
+	GEMCMD_COMMANDS         = "commands"
+	GENCMD_MEASURE_TEMP     = "measure_temp"
+	GENCMD_MEASURE_CLOCK    = "measure_clock arm core h264 isp v3d uart pwm emmc pixel vec hdmi dpi"
+	GENCMD_MEASURE_VOLTS    = "measure_volts core sdram_c sdram_i sdram_p"
+	GENCMD_CODEC_ENABLED    = "codec_enabled H264 MPG2 WVC1 MPG4 MJPG WMV9 VP8"
+	GENCMD_MEMORY           = "get_mem arm gpu"
+	GENCMD_OTPDUMP          = "otp_dump"
+	GENCMD_OTPDUMP_SERIAL   = 28
+	GENCMD_OTPDUMP_REVISION = 30
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func BCMHostInit() {
+	C.bcm_host_init()
+}
+
+func BCMHostTerminate() {
+	C.bcm_host_deinit()
+}
+
+func GraphicsGetDisplaySize(displayNumber uint16) (uint32, uint32) {
+	var w, h uint32
+	C.graphics_get_display_size((C.uint16_t)(displayNumber), (*C.uint32_t)(&w), (*C.uint32_t)(&h))
+	return w, h
+}
+
+func VCGenCmdInit() error {
+	if C.vc_gencmd_init() >= 0 {
+		return nil
+	}
+	return ErrorInit
+}
+
+func VCGenCmdStop() {
+	C.vc_gencmd_stop()
+}
+
+// See http://elinux.org/RPI_vcgencmd_usage for some example usage
+func VCGenCmd(command string) (string, error) {
+	ccommand := C.CString(command)
+	defer C.free(unsafe.Pointer(ccommand))
+	cbuffer := make([]byte, GENCMD_BUFFER_SIZE)
+	if int(C.vc_gencmd_wrapper((*C.char)(unsafe.Pointer(&cbuffer[0])), C.int(GENCMD_BUFFER_SIZE), (*C.char)(unsafe.Pointer(ccommand)))) != 0 {
+		return "", ErrorGenCmd
+	}
+	return string(cbuffer), nil
+}
