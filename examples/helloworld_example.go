a91d4e9086d6b2994926951a1644430c8ecb3a7a
David Thorpe <djt@mutablelogic.com>
2016-10-27 19:21:21 +0100

    Updated helloworld example

--- examples/helloworld_example.go
+++ examples/helloworld_example.go
@@ -7,8 +7,39 @@
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+import (
+	app "../app"   /* import "github.com/djthorpe/gopi/app" */
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func HelloWorld(app *app.App) error {
+	app.Logger.Info("Hello, World")
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	// Create the application
+	myapp, err := app.NewApp(app.AppConfig{
+		Features:  app.APP_DEVICE,
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		return
+	}
+	defer myapp.Close()
+
+	// Run the application
+	if err := myapp.Run(HelloWorld); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		return
+	}
 }
