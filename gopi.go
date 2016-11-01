c50e6c7794e2e1fc97e3ae05d3f3ea654b5fb717
David Thorpe <davidthorpe@google.com>
2016-11-01 09:49:30 +0000

    Updated GPIO so it goes into hw module

--- gopi.go
+++ gopi.go
@@ -65,79 +65,12 @@ type DisplayDriver interface {
 	Driver
 }
 
-// Abstract GPIO interface
-type GPIODriver interface {
-	// Enforces general driver
-	Driver
-
-	// Return array of available logical pins
-	Pins() []GPIOPin
-
-	// Return logical pin for physical pin number. Returns
-	// GPIO_PIN_NONE where there is no logical pin at that position
-	PhysicalPin(uint) GPIOPin
-
-	// Return physical pin number for logical pin. Returns 0 where there
-	// is no physical pin for this logical pin
-	PhysicalPinForPin(GPIOPin) uint
-
-	// Read pin state
-	ReadPin(GPIOPin) GPIOState
-
-	// Write pin state
-	WritePin(GPIOPin,GPIOState)
-
-	// Get pin mode
-	GetPinMode(GPIOPin) GPIOMode
-
-	// Set pin mode
-	SetPinMode(GPIOPin,GPIOMode)
-
-}
-
 // Abstract configuration which is used to open and return the
 // concrete driver
 type Config interface {
 	// Opens the driver from configuration, or returns error
 	Open(*util.LoggerDevice) (Driver, error)
 }
-
-// GPIO types
-type (
-	GPIOPin uint8
-	GPIOState uint8
-	GPIOMode uint8
-	GPIOPull uint8
-)
-
-////////////////////////////////////////////////////////////////////////////////
-// GPIO CONSTANTS
-
-const (
-	GPIO_PIN_NONE GPIOPin = 0xFF
-)
-
-const (
-	GPIO_LOW GPIOState = iota
-	GPIO_HIGH
-)
-
-const (
-	GPIO_INPUT GPIOMode = iota
-	GPIO_OUTPUT
-	GPIO_ALT5
-	GPIO_ALT4
-	GPIO_ALT0
-	GPIO_ALT1
-	GPIO_ALT2
-	GPIO_ALT3
-)
-
-const (
-	GPIO_PULL_OFF GPIOPull = iota
-	GPIO_PULL_DOWN
-	GPIO_PULL_UP
-)
 
 ////////////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS: Config interface implementation
@@ -158,57 +91,4 @@ func Open(config Config,log *util.LoggerDevice) (Driver, error) {
 	}
 	return driver, nil
 }
-
-func (p GPIOPin) String() string {
-	return fmt.Sprintf("GPIO%v",uint8(p))
-}
-
-func (s GPIOState) String() string {
-	switch(s) {
-	case GPIO_LOW:
-		return "LOW"
-	case GPIO_HIGH:
-		return "HIGH"
-	default:
-		return "[??? Invalid GPIOState value]"
-	}
-}
-
-func (m GPIOMode) String() string {
-	switch(m) {
-	case GPIO_INPUT:
-		return "INPUT"
-	case GPIO_OUTPUT:
-		return "OUTPUT"
-	case GPIO_ALT0:
-		return "ALT0"
-	case GPIO_ALT1:
-		return "ALT1"
-	case GPIO_ALT2:
-		return "ALT2"
-	case GPIO_ALT3:
-		return "ALT3"
-	case GPIO_ALT4:
-		return "ALT4"
-	case GPIO_ALT5:
-		return "ALT5"
-	default:
-		return "[??? Invalid GPIOMode value]"
-	}
-}
-
-func (p GPIOPull) String() string {
-	switch(p) {
-	case GPIO_PULL_OFF:
-		return "PULL_OFF"
-	case GPIO_PULL_DOWN:
-		return "PULL_DOWN"
-	case GPIO_PULL_UP:
-		return "PULL_UP"
-	default:
-		return "[??? Invalid GPIOPull value]"
-	}
-}
-
-
 
---
+++ hw/gpio.go
@@ -0,0 +1,154 @@
+/*
+	Go Language Raspberry Pi Interface
+	(c) Copyright David Thorpe 2016
+	All Rights Reserved
+
+	For Licensing and Usage information, please see LICENSE.md
+*/
+
+// This package file implements the abstract hardware interfaces for GPIO
+package hw // import "github.com/djthorpe/gopi/hw"
+
+import (
+	"fmt"
+)
+
+import (
+	gopi "../" // import "github.com/djthorpe/gopi"
+	util "../util" // import "github.com/djthorpe/gopi/util"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// TYPES
+
+// Abstract GPIO interface
+type GPIODriver interface {
+	// Enforces general driver
+	Driver
+
+	// Return array of available logical pins
+	Pins() []GPIOPin
+
+	// Return logical pin for physical pin number. Returns
+	// GPIO_PIN_NONE where there is no logical pin at that position
+	PhysicalPin(uint) GPIOPin
+
+	// Return physical pin number for logical pin. Returns 0 where there
+	// is no physical pin for this logical pin
+	PhysicalPinForPin(GPIOPin) uint
+
+	// Read pin state
+	ReadPin(GPIOPin) GPIOState
+
+	// Write pin state
+	WritePin(GPIOPin,GPIOState)
+
+	// Get pin mode
+	GetPinMode(GPIOPin) GPIOMode
+
+	// Set pin mode
+	SetPinMode(GPIOPin,GPIOMode)
+
+}
+
+// GPIO types
+type (
+	// Logical GPIO pin
+	GPIOPin uint8
+
+	// GPIO Pin state
+	GPIOState uint8
+
+	// GPIO Pin mode
+	GPIOMode uint8
+
+	// GPIO Pin resistor configuration (pull up/down or floating)
+	GPIOPull uint8
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// GPIO CONSTANTS
+
+const (
+	// Invalid pin constant
+	GPIO_PIN_NONE GPIOPin = 0xFF
+)
+
+const (
+	GPIO_LOW GPIOState = iota
+	GPIO_HIGH
+)
+
+const (
+	GPIO_INPUT GPIOMode = iota
+	GPIO_OUTPUT
+	GPIO_ALT5
+	GPIO_ALT4
+	GPIO_ALT0
+	GPIO_ALT1
+	GPIO_ALT2
+	GPIO_ALT3
+)
+
+const (
+	GPIO_PULL_OFF GPIOPull = iota
+	GPIO_PULL_DOWN
+	GPIO_PULL_UP
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC METHODS
+
+func (p GPIOPin) String() string {
+	return fmt.Sprintf("GPIO%v",uint8(p))
+}
+
+func (s GPIOState) String() string {
+	switch(s) {
+	case GPIO_LOW:
+		return "LOW"
+	case GPIO_HIGH:
+		return "HIGH"
+	default:
+		return "[??? Invalid GPIOState value]"
+	}
+}
+
+func (m GPIOMode) String() string {
+	switch(m) {
+	case GPIO_INPUT:
+		return "INPUT"
+	case GPIO_OUTPUT:
+		return "OUTPUT"
+	case GPIO_ALT0:
+		return "ALT0"
+	case GPIO_ALT1:
+		return "ALT1"
+	case GPIO_ALT2:
+		return "ALT2"
+	case GPIO_ALT3:
+		return "ALT3"
+	case GPIO_ALT4:
+		return "ALT4"
+	case GPIO_ALT5:
+		return "ALT5"
+	default:
+		return "[??? Invalid GPIOMode value]"
+	}
+}
+
+func (p GPIOPull) String() string {
+	switch(p) {
+	case GPIO_PULL_OFF:
+		return "PULL_OFF"
+	case GPIO_PULL_DOWN:
+		return "PULL_DOWN"
+	case GPIO_PULL_UP:
+		return "PULL_UP"
+	default:
+		return "[??? Invalid GPIOPull value]"
+	}
+}
+
+
+
