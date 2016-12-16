3a9eb9c65e1798ff484d61ee7a5cb13b957a704a
David Thorpe <djt@mutablelogic.com>
2016-12-16 12:13:34 +0000

    Added PPI and display example

---
+++ examples/display/display_example.go
@@ -0,0 +1,45 @@
+/*
+	Go Language Raspberry Pi Interface
+	(c) Copyright David Thorpe 2016
+	All Rights Reserved
+
+	For Licensing and Usage information, please see LICENSE.md
+*/
+
+// This example opens a display and returns information about the display.
+package main
+
+import (
+	app "github.com/djthorpe/gopi/app"
+	khronos "github.com/djthorpe/gopi/khronos"
+	util "github.com/djthorpe/gopi/util"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func MyRunLoop(app *app.App) error {
+	app.Loger.Info("RUN LOOP")
+
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func main() {
+	// Create the config
+	config := app.Config(app.APP_DISPLAY)
+
+	// Create the application
+	myapp, err := app.NewApp(config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		return
+	}
+	defer myapp.Close()
+
+	// Run the application
+	if err := myapp.Run(MyRunLoop); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		return
+	}
+}
