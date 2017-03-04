4c9d5a85ece29eb2f92a412dfce0cd7e9e96796d
David Thorpe <davidthorpe@google.com>
2017-03-04 14:06:37 +0000

    Added Open2 method

--- gopi.go
+++ gopi.go
@@ -6,28 +6,6 @@
 	For Licensing and Usage information, please see LICENSE.md
 */
 
-// Package gopi implements a Golang interface for the Raspberry Pi. It's
-// a bag of interfaces to both the Broadcom ARM processor and to various
-// devices which can be plugged in and interfaced via various interfaces.
-//
-// Start by creating a gopi object as follows:
-//
-//   import "github.com/djthorpe/gopi/device/rpi"
-//
-//   device, err := gopi.Open(rpi.Device{ /* configuration */ })
-//   if err != nil { /* handle error */ }
-//
-// You should then have an object which can be used to retrieve information
-// about the Raspberry Pi (serial number, memory configuration, temperature,
-// and so forth). When you're done with the object you should release the
-// resources using the following method:
-//
-//   if err := device.Close(); err != nil { /* handle error */ }
-//
-// You'll need a configuration from the "concrete device". In this case, it's
-// the Raspberry Pi device driver. The use these abstrat interfaces will allow
-// for other devices to be implemented a bit differently in the future.
-//
 package gopi // import "github.com/djthorpe/gopi"
 
 import (
@@ -116,7 +94,7 @@ func Open(config Config, log *util.LoggerDevice) (Driver, error) {
 
 // Open a driver - opens the concrete version given the config method
 // and only returns the driver (or nil). Will return an error as a
-// passes argument.
+// reference.
 func Open2(config Config, log Logger, error_ref *Error) Driver {
 	var err error
 	var driver Driver
