a2cc4b008ae63c25c04e26bbe186d3d585603e6d
David Thorpe <djt@mutablelogic.com>
2017-08-07 17:05:03 +0100

    Added layout engine to gopi

---
+++ sys/default/logger/logger.go
@@ -0,0 +1,58 @@
+package mock /* import "github.com/djthorpe/gopi/sys/default/logger" */
+
+import (
+	"github.com/djthorpe/gopi"
+	"github.com/djthorpe/gopi/util"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// INIT
+
+func init() {
+	// Register logger
+	registerLoggerFlags(gopi.RegisterModule(gopi.Module{
+		Name: "default/logger",
+		Type: gopi.MODULE_TYPE_LOGGER,
+		New:  newLogger,
+	}))
+}
+
+func registerLoggerFlags(flags *util.Flags) {
+	flags.FlagString("log", "", "File for logging (default: log to stderr)")
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE METHODS
+
+func newLogger(config *gopi.AppConfig, _ gopi.Logger) (gopi.Driver, error) {
+	logger, err := getLoggerForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	logger.SetLevel(getLevelForConfig(config))
+	return logger, nil
+}
+
+func getLoggerForConfig(config *gopi.AppConfig) (*util.LoggerDevice, error) {
+	file, exists := config.Flags.GetString("log")
+	if exists {
+		return util.Logger(util.FileLogger{Filename: file, Append: false})
+	} else {
+		return util.Logger(util.StderrLogger{})
+	}
+}
+
+func getLevelForConfig(config *gopi.AppConfig) util.LogLevel {
+	if config.Debug {
+		if config.Verbose {
+			return util.LOG_DEBUG2
+		} else {
+			return util.LOG_DEBUG
+		}
+	}
+	if config.Verbose {
+		return util.LOG_INFO
+	} else {
+		return util.LOG_WARN
+	}
+}
