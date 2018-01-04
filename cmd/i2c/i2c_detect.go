e79e92ab7747465c7f876210a6aba61928e6e2bf
David Thorpe <djt@mutablelogic.com>
2018-01-04 09:17:30 +0000

    Fixed i2c detect with header

--- cmd/i2c/i2c_detect.go
+++ cmd/i2c/i2c_detect.go
@@ -30,7 +30,7 @@ func mainLoop(app *gopi.AppInstance, done chan<- struct{}) error {
 	// Output detected I2C addresses
 	table := tablewriter.NewWriter(os.Stdout)
 
-	table.Append([]string{"", "-0", "-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-A", "-B", "-C", "-D", "-E", "-F"})
+	table.SetHeader([]string{"", "-0", "-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9", "-A", "-B", "-C", "-D", "-E", "-F"})
 	row := make([]string, 0)
 
 	for slave := uint8(0); slave < 0x80; slave++ {
