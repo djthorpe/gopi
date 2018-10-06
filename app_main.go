eb9c1ca994d537b7f39ec919a9b30a84423942b6
David Thorpe <djt@mutablelogic.com>
2018-10-06 16:26:39 +0100

    Added Run2 method temporarily

--- app_main.go
+++ app_main.go
@@ -47,6 +47,33 @@ func CommandLineTool(config AppConfig, main_task MainTask, background_tasks ...B
 	return 0
 }
 
+// CommandLineTool2 runs multiple background tasks, waits for them all to send a
+// start signl and then runs the main task
+func CommandLineTool2(config AppConfig, main_task MainTask, background_tasks ...BackgroundTask2) int {
+
+	// Create the application
+	app, err := NewAppInstance(config)
+	if err != nil {
+		if err != ErrHelp {
+			fmt.Fprintln(os.Stderr, err)
+			return -1
+		}
+		return 0
+	}
+	defer app.Close()
+
+	// Start main task
+	if err := app.Run2(main_task, background_tasks...); err == ErrHelp {
+		config.AppFlags.PrintUsage()
+		return 0
+	} else if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return -1
+	} else {
+		return 0
+	}
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // RPC SERVER STARTUP
 
---
+++ cmd/tasks/tasks.go
@@ -0,0 +1,76 @@
+/*
+	Go Language Raspberry Pi Interface
+	(c) Copyright David Thorpe 2016-2018
+	All Rights Reserved
+	Documentation http://djthorpe.github.io/gopi/
+	For Licensing and Usage information, please see LICENSE.md
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	// Frameworks
+	"github.com/djthorpe/gopi"
+
+	// Modules
+	_ "github.com/djthorpe/gopi/sys/logger"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func Background2(app *gopi.AppInstance, start chan<- struct{}, stop <-chan struct{}) error {
+	app.Logger.Info("Waiting on Background2...")
+	time.Sleep(time.Second)
+	// Send start signal, to show initialization has happened
+	start <- gopi.DONE
+	app.Logger.Info("Started Background2...")
+
+FOR_LOOP:
+	for {
+		select {
+		case <-stop:
+			break FOR_LOOP
+		}
+	}
+
+	app.Logger.Info("Finished Background2...")
+	return fmt.Errorf("Error from Background 2")
+
+}
+
+func Background1(app *gopi.AppInstance, start chan<- struct{}, stop <-chan struct{}) error {
+	// Send start signal, to show initialization has happened
+	start <- gopi.DONE
+	app.Logger.Info("Started Background1...")
+
+FOR_LOOP:
+	for {
+		select {
+		case <-stop:
+			break FOR_LOOP
+		}
+	}
+
+	app.Logger.Info("Finished Background1...")
+	return fmt.Errorf("Error from Background 1")
+}
+
+func Main(app *gopi.AppInstance, done chan<- struct{}) error {
+	app.Logger.Info("Started Main...")
+	time.Sleep(time.Second * 2)
+
+	app.Logger.Info("Sending DONE signal from main...")
+	done <- gopi.DONE
+
+	app.Logger.Info("...Finished Main")
+	return fmt.Errorf("Error from Main")
+}
+
+func main() {
+	// Run the command line tool
+	os.Exit(gopi.CommandLineTool2(gopi.NewAppConfig(), Main, Background1, Background2))
+}
