459ef0702c6bebba06dbf17b9bd2a040446a32eb
David Thorpe <djt@mutablelogic.com>
2020-10-05 19:03:13 +1030

    Added waveshare device

---
+++ pkg/dev/waveshare/epd.go
@@ -0,0 +1,20 @@
+package waveshare
+
+import "github.com/djthorpe/gopi/v3"
+
+type EPD struct {
+	gopi.Unit
+	*gpiobcm.GPIO
+}
+
+const (
+	EPD_PIN_RESET = gopi.GPIOPin(17)
+	EPD_PIN_CS    = gopi.GPIOPin(8)
+	EPD_PIN_DC    = gopi.GPIOPin(25)
+	EPD_PIN_BUSY  = gopi.GPIOPin(24)
+
+	EPD_SPI_SPEED = 10000000
+	EPD_SPI_BUS   = 0
+	EPD_SPI_SLAVE = 0
+	EPD_SPI_MODE  = gopi.SPI_MODE_0
+)
