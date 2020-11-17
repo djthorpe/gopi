1fc6c6cc74fc54e7505b57948e812d33e2220360
David Thorpe <djt@mutablelogic.com>
2020-11-17 18:48:35 +0100

    Added metrics dumping

--- cmd/metrics/app.go
+++ cmd/metrics/app.go
@@ -19,6 +19,7 @@ type app struct {
 
 func (this *app) Define(cfg gopi.Config) error {
 	cfg.Command("scan", "Scan a CSV file and display definition", this.Scan)
+	cfg.Command("dump", "Display CSV file", this.Dump)
 	return nil
 }
 
@@ -63,9 +64,51 @@ func (this *app) Scan(ctx context.Context) error {
 		} else if err != nil {
 			return err
 		}
-
 		table.Scan(record)
 	}
+
+	// Write out the table
+	table.Schema().Write(os.Stdout)
+
+	// Return success
+	return nil
+}
+
+func (this *app) Dump(ctx context.Context) error {
+	// Check argument
+	args := this.Command.Args()
+	if len(args) != 1 {
+		return gopi.ErrBadParameter.WithPrefix(this.Command.Name())
+	} else if stat, err := os.Stat(args[0]); os.IsNotExist(err) {
+		return err
+	} else if stat.Mode().IsRegular() == false {
+		return gopi.ErrBadParameter.WithPrefix(args[0])
+	} else if err != nil {
+		return err
+	}
+
+	// Create a table for scanning
+	table := NewTable(true)
+
+	// Open file for CSV parsing
+	fh, err := os.Open(args[0])
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+	csv := csv.NewReader(fh)
+	for {
+		record, err := csv.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		table.Append(record)
+	}
+
+	// Write out the table
+	table.Write(os.Stdout)
 
 	// Return success
 	return nil
