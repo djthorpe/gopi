e6378c0e7bdf3be478a1d1b633ccda450169dd81
David Thorpe <djt@mutablelogic.com>
2020-11-18 11:16:30 +0100

    Added serve and version commands to pingserver

--- cmd/rpc/pingserver/app.go
+++ cmd/rpc/pingserver/app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
+	"time"
 
 	"github.com/djthorpe/gopi/v3"
 )
@@ -12,14 +14,74 @@ type app struct {
 	gopi.Server
 	gopi.PingService
 	gopi.Logger
+	gopi.Command
+}
+
+func (this *app) Define(cfg gopi.Config) error {
+	cfg.Command("version", "Return version information", this.Version(cfg.Version()))
+	cfg.Command("serve", "Start server", this.Serve)
+	return nil
+}
+
+func (this *app) New(cfg gopi.Config) error {
+	if this.Command = cfg.GetCommand(cfg.Args()); this.Command == nil {
+		return gopi.ErrHelp
+	}
+	return nil
 }
 
 func (this *app) Run(ctx context.Context) error {
-	if err := this.Server.StartInBackground("tcp", ":0"); err != nil {
+	return this.Command.Run(ctx)
+}
+
+func (this *app) Version(v gopi.Version) func(context.Context) error {
+	return func(context.Context) error {
+		if name := v.Name(); name != "" {
+			fmt.Printf("%-10s %s\n", "Name", v.Name())
+		}
+		tag, branch, hash := v.Version()
+		if tag != "" {
+			fmt.Printf("%-10s %s\n", "Tag", tag)
+		}
+		if branch != "" {
+			fmt.Printf("%-10s %s\n", "Branch", branch)
+		}
+		if hash != "" {
+			fmt.Printf("%-10s %s\n", "Hash", hash)
+		}
+		if buildtime := v.BuildTime(); buildtime.IsZero() == false {
+			fmt.Printf("%-10s %s\n", "BuildTime", buildtime.Format(time.RFC3339))
+		}
+		if version := v.GoVersion(); version != "" {
+			fmt.Printf("%-10s %s\n", "GoVersion", version)
+		}
+	}
+}
+
+func (this *app) Serve(ctx context.Context) error {
+	var network, addr string
+
+	args := this.Command.Args()
+	switch {
+	case len(args) == 0:
+		network = "tcp"
+		addr = ":0"
+	case len(args) == 1:
+		if _, _, err := net.SplitHostPort(args[0]); err == nil {
+			network = "tcp"
+			addr = args[0]
+			break
+		}
+		fallthrough
+	default:
+		return gopi.ErrHelp.WithPrefix(this.Command.Name())
+	}
+
+	if err := this.Server.StartInBackground(network, addr); err != nil {
 		return err
 	}
 
-	fmt.Println("Started Ping Server, ", this.Server)
+	fmt.Println("Started server, ", this.Server)
 	fmt.Println("Press CTRL+C to end")
 
 	// Wait until done
