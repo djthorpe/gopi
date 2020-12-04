de7fbf9772cef3ace6f9d476cff0fee999a2b240
David Thorpe <djt@mutablelogic.com>
2020-12-04 23:32:02 +0100

    Updated tests

--- pkg/mdns/discovery_test.go
+++ pkg/mdns/discovery_test.go
@@ -1,21 +1,22 @@
 package mdns_test
 
 import (
+	"context"
 	"sync"
 	"testing"
 	"time"
 
 	gopi "github.com/djthorpe/gopi/v3"
-	_ "github.com/djthorpe/gopi/v3/pkg/event"
-	mdns "github.com/djthorpe/gopi/v3/pkg/mdns"
 	tool "github.com/djthorpe/gopi/v3/pkg/tool"
-	"golang.org/x/net/context"
+
+	_ "github.com/djthorpe/gopi/v3/pkg/event"
+	_ "github.com/djthorpe/gopi/v3/pkg/mdns"
 )
 
 type DiscoveryApp struct {
 	gopi.Unit
 	gopi.Logger
-	*mdns.Discovery
+	gopi.ServiceDiscovery
 }
 
 func (this *DiscoveryApp) Run(ctx context.Context) error {
@@ -25,14 +26,8 @@ func (this *DiscoveryApp) Run(ctx context.Context) error {
 
 func Test_Discovery_001(t *testing.T) {
 	tool.Test(t, nil, new(DiscoveryApp), func(app *DiscoveryApp) {
-		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-		defer cancel()
-
-		// Cancel after one second
-		if services, err := app.Discovery.EnumerateServices(ctx); err != nil {
-			t.Error("EnumerateServices:", err)
-		} else {
-			t.Log("EnumerateServices:", services)
+		if app.ServiceDiscovery == nil {
+			t.Error("No ServiceDiscovery object")
 		}
 	})
 }
@@ -43,7 +38,21 @@ func Test_Discovery_002(t *testing.T) {
 		defer cancel()
 
 		// Cancel after one second
-		if services, err := app.Discovery.EnumerateServices(ctx); err != nil {
+		if services, err := app.ServiceDiscovery.EnumerateServices(ctx); err != nil {
+			t.Error("EnumerateServices:", err)
+		} else {
+			t.Log("EnumerateServices:", services)
+		}
+	})
+}
+
+func Test_Discovery_003(t *testing.T) {
+	tool.Test(t, nil, new(DiscoveryApp), func(app *DiscoveryApp) {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		// Cancel after one second
+		if services, err := app.ServiceDiscovery.EnumerateServices(ctx); err != nil {
 			t.Error("EnumerateServices:", err)
 		} else {
 			var wg sync.WaitGroup
@@ -53,7 +62,7 @@ func Test_Discovery_002(t *testing.T) {
 					defer wg.Done()
 					ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 					defer cancel()
-					if r, err := app.Discovery.Lookup(ctx, service); err != nil {
+					if r, err := app.ServiceDiscovery.Lookup(ctx, service); err != nil {
 						t.Error(err)
 					} else {
 						t.Log("Lookup:", service, r)
